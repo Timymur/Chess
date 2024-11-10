eb7b404a275ccdea6fe773c25db3b48cf4afb9f7
Timymur <hitechmag2013@mail.ru>
2024-11-10 16:48:17 +0500

    logic

---
+++ Figure.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    enum Figure
+    {
+        none,
+
+        whiteKing = 'K',
+        whiteQueen = 'Q',
+        whiteKnight = 'N',
+        whiteBishop = 'B',
+        whiteRook = 'R',
+        whitePawn = 'P',
+
+        blackKing = 'k',
+        blackQueen = 'q',
+        blackKnight = 'n',
+        blackBishop = 'b',
+        blackRook = 'r',
+        blackePawn = 'p',
+
+
+    }
+
+    static class FigureMethods
+    {
+        public static Color GetColor(this Figure figure)
+        {
+            if (figure == Figure.none)
+                return Color.none;
+            return (figure == Figure.whiteKing ||
+                    figure == Figure.whiteQueen ||
+                    figure == Figure.whiteKnight ||
+                    figure == Figure.whiteBishop ||
+                    figure == Figure.whiteRook ||
+                    figure == Figure.whitePawn)
+                ? Color.white
+                : Color.black;
+        }
+    }
+
+}
---
+++ FigureMoving.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    internal class FigureMoving
+    {
+        public Figure figure {  get; private set; }
+        public Square from { get; private set; }
+        public Square to { get; private set; }
+
+        public Figure promotion { get; private set; }
+
+        public FigureMoving(FigureOnSquare fs, Square to, Figure promotion = Figure.none)
+        {
+            this.figure = fs.figure;
+            this.from = fs.square;
+            this.to = to;
+            this.promotion = promotion;
+        }
+
+        public FigureMoving(string move) // Pe2e4     pe7e8Q
+        {                                // 01234     012345
+            this.figure = (Figure)move[0];  //Сразу меняем тип данных с чар на Figure
+            this.from = new Square(move.Substring(1 , 2));
+            this.to = new Square(move.Substring(3, 2));
+            this.promotion = (move.Length == 6) ? (Figure)move[5] : Figure.none; // Если длина строки равна 6, то мы берем пятый элемент , иначе нан
+
+        }
+
+        public int DeltaX { get { return to.x - from.x; } }
+        public int DeltaY { get { return to.y - from.y; } }
+
+        public int AbsDeltaX { get { return Math.Abs(DeltaX); } }
+        public int AbsDeltaY { get { return Math.Abs(DeltaY); } }
+
+        public int SignX { get { return Math.Sign(DeltaX); } }
+        public int SignY { get { return Math.Sign(DeltaY); } }
+
+        public override string ToString()
+        {
+            string text = (char)figure + from.Name + to.Name;
+            if (promotion != Figure.none)
+                text += (char)promotion;
+            return text;
+        }
+
+    }
+}
